e1671a1efd2be02760959692c22d967218a4ea76
AbbyShancock <asaucedo44@cps.edu>
2025-01-07 14:48:27 -0600

    Update 1_.py

--- 1_.py
+++ 1_.py
@@ -1,34 +1,66 @@
-# Methods, Help & Documentation Practice #1
-# Remove the characters to the left of our main text:
+from determineEligibility import determineEligibility_toGraduate, listOfMovies
+# #functions are always to wrap up your code
+# #into reusable units
 
-# ,
-
-# :
-
-# %
-
-# _
-
-# #
-
-# Use the lstrip() method. Print the result to the screen:
-
-# ",:_#,,,,,,:::____##Total_ _Pyt%on,,,,,,::#"
-
-# Search the documentation for the requested method to learn how it works. You can use intermediate variables if you need them.
+# #define a function with def
+# #function names end in ()
+# #when I pass in a parameter,
+# #i am passing in a placeholder
+# #for future information (name)
 
 
-# Methods, Help & Documentation Practice #2
-# Add the element "orange" as the fourth element of the following list fruits, using the insert() method:
+def say_hello(name,age,address):
+    print(f"Hello!{name}")
+    print(f"How are you?{name}")
+    print(f"{name} are {age} years old")
+    print(f"{name} loves in {address}")
 
-# fruits = ["mango", "banana", "cherry", "plum", "grapefruit"]
+# #call the function
+# #pass in information called arguement (Alice)
+#     #pass a second arguement for age
+#     #pass a third arguemnet for address
+#     #three parameters, three arguements.
 
-# Search the documentation for the requested method to know how it works.
 
-# Methods, Help & Documentation Practice #3
-# Check if the sets below are isolated (that is, they have no elements in common), using the isdisjoint() method. Store this result in the isolated_sets variable:
+say_hello("Alice",22,"123 Main St.") 
+say_hello("Paul",34, "456 Main St")
+say_hello("Bob",56,"789 Main St")
+say_hello("Altair",45,"101 Main St")
 
-# phone_brands = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
-# tv_brands = {"Sony", "Philips", "Samsung", "LG"}
-# Search the documentation for the requested method to know how it works.
 
+
+determineEligibility_toGraduate("Alice", 120, 2.0, 800)
+determineEligibility_toGraduate("Bob", 119, 1.9, 799)
+
+listOfMovies("the Matrix", 10, "action")
+listOfMovies("The Hangover", 5, "comedy")
+
+
+
+
+
+#the return statement is
+#used to return a value from a function
+#return statements are unsed to end a function and
+#send a result back to the caller
+
+def add(x, y):
+    z = x + y
+    return z
+
+def subtract(x, y):
+    z = x - y
+    return z
+
+def multiply(x, y):
+    z = x * y
+    return z
+
+def divide(x, y):
+    z = x / y
+    return z
+
+print(add(1, 2))
+print(subtract(1, 2))
+print(multiply(1, 2))
+print(divide(1, 2))
